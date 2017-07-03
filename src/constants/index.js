a0422f548b7d1c2049a162f36c3ebc84e4d530c2
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-07-03 07:50:14 +0300

    Fix type of API KEY

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,6 @@ const API_KEY = "55731a29a05ead0f8d69b0cf66774cf9";
 
 const FETCH_WEATHER = "FETCH_WEATHER";
 
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?&appid=${APIKEY}`;
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
 
 export { API_KEY, ROOT_URL, FETCH_WEATHER };
