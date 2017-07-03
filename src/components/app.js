e933eaf57129e181d9508177d37a6f3d5c12ab46
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-07-03 10:12:52 +0300

    add weather list container

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
 import SearchBar from "../container/SearchBar";
+import WeatherList from "../container/WeatherList";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <SearchBar />
+        <WeatherList />
       </div>
     );
   }
