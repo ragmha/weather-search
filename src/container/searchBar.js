17801f7b6f72f6b63684c8d2ea6ef0b84bf633c5
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-07-03 07:09:43 +0300

    added controlled component, binding context with event handlers

--- src/container/searchBar.js
+++ src/container/searchBar.js
@@ -2,15 +2,36 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class searchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: "" };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div>
-        <input />
-        <span className="input-group-btn" />
-        <button type="submit" className="btn btn-secondary">
-          Submit
-        </button>
-      </div>
+      <form className="input-group" onSubmit={this.onFormSubmit}>
+        <input
+          placeholder="Get a Five day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">
+            Submit
+          </button>
+        </span>
+      </form>
     );
   }
 }
